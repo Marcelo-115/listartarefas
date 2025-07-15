09b5dbe187b4e2365b2d5682354d16cc89bed0e1
Marcelo-115 <108548027+Marcelo-115@users.noreply.github.com>
2025-07-14 22:31:46 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+/*
+[x]saber quando o botao foi clicado
+[x]pegar texto dentro do input
+[x]colocar esse texto na tela
+[x]Deletar tarefa ao clicar no x
+*/ 
+
+function adicionarTarefa(){
+    let tarefa = document.querySelector("input").value
+   
+    let li = document.createElement('li')
+    li.innerHTML = tarefa + '<span onclick="deletarTarefa(this)">❌</span>'
+    li.setAttribute("onclick", "concluirTarefa(this)");
+   
+    document.querySelector('ul').appendChild(li)
+   
+    document.querySelector("input").value = ''
+
+};
+
+function deletarTarefa(li){
+    li.parentElement.remove()
+}
+
+function concluirTarefa(li){
+    li.classList.toggle("riscado")
+}
+
